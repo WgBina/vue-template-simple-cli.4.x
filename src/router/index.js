63cd8c29057152a0b530c903ef04c5f1c2918e25
bing.wan <1078827038@qq.com>
2020-05-25 16:34:58 +0800

    添加配置项

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,10 @@
-import Vue from 'vue'
-import Router from 'vue-router'
+import Vue from 'vue';
+import Router from 'vue-router';
 
-Vue.use(Router)
+Vue.use(Router);
 
 /* Layout */
-import Layout from '@/layout'
+import Layout from '@/layout';
 
 /**
  * constantRoutes
@@ -12,45 +12,49 @@ import Layout from '@/layout'
  * all roles can be accessed
  */
 const router = new Router({
-  routes: [
-    {
-      path: '/login',
-      component: () => import('@/views/login/index'),
-      hidden: true
-    },
-    {
-      path: '/',
-      name: 'Layout',
-      component: Layout,
-      redirect: '/test1',
-      meta: { title: 'head1', icon: 'link' },
-      children: [
+    routes: [
         {
-          path: 'test1',
-          component: () => import('@/views/test1'),
-          name: 'test1',
-          meta: { title: 'test1', icon: 'link' },
-          children: [
-            {
-              path: 'test3',
-              component: () => import('@/views/test2'),
-              name: 'test3',
-              meta: { title: 'test3', icon: 'link', activeMenu: '/test1' },
-              hidden: true
-            }
-          ]
+            path: '/login',
+            component: () => import('@/views/login/index'),
+            hidden: true
         },
         {
-          path: 'test2',
-          component: () => import('@/views/test2'),
-          name: 'test2',
-          meta: { title: 'test2', icon: 'link' }
+            path: '/',
+            name: 'Layout',
+            component: Layout,
+            redirect: '/test1',
+            meta: { title: 'head1', icon: 'link' },
+            children: [
+                {
+                    path: 'test1',
+                    component: () => import('@/views/test1'),
+                    name: 'test1',
+                    meta: { title: 'test1', icon: 'link' },
+                    children: [
+                        {
+                            path: 'test3',
+                            component: () => import('@/views/test2'),
+                            name: 'test3',
+                            meta: {
+                                title: 'test3',
+                                icon: 'link',
+                                activeMenu: '/test1'
+                            },
+                            hidden: true
+                        }
+                    ]
+                },
+                {
+                    path: 'test2',
+                    component: () => import('@/views/test2'),
+                    name: 'test2',
+                    meta: { title: 'test2', icon: 'link' }
+                }
+            ]
         }
-      ]
-    }
-    // 404 page must be placed at the end !!!
-    // { path: '*', redirect: '/404', hidden: true }
-  ]
-})
+        // 404 page must be placed at the end !!!
+        // { path: '*', redirect: '/404', hidden: true }
+    ]
+});
 
-export default router
+export default router;
