c3adb7e6186ea4233e3908d5a380c75d0624be91
bing.wan <1078827038@qq.com>
2020-11-09 13:28:03 +0800

    修改配置文件

--- src/utils/dictionary.js
+++ src/utils/dictionary.js
@@ -1,68 +1,70 @@
-import qs from "qs";
-import axios from "axios";
+import qs from 'qs';
+import axios from 'axios';
+
 /**
  * 若项目中有埋点需求，可以引入
-*/
+ */
+const http = 'https://dht.colourdata.com.cn/EventTracking';
 const dictionary = {
-    projectId: 'If903a96f-672a-40b9-a148-bd1ad79dde35',
+    projectId: 'Ic4b46dad-3236-4a4b-a963-8c3098b8258d',
     pageId: {
-        home: 'Pf541bee1-334f-4dbf-a77e-01d86561056d', //首页
-        success: 'P9436c1fb-1952-4e85-a64a-c529f8de2b32', //参与成功页
+        loading: 'P6a0e4a08-9221-4cd6-8890-8e1a3b267167', //
+        bell: 'P6ab986e2-ad89-498f-a922-620b1deceae8', //
+        list: 'P2b5aa2fa-bf5d-4e41-9514-e342e41f964d' //
     },
     controlId: {
-        立即参与: 'C3452fd9d-dafd-4e6a-bce6-45757a7c7bca',
+        accpet: 'Cd2a3eebb-4109-476f-8456-d470b6816b16', //
+        list: 'Cf96d457f-5751-4181-b34f-fa0cda99dd05' //
     },
 
-    equipment:'H5'
+    equipment: 'H5'
 };
-
-
-
-function  mai(params = {}) {
-    //名字换取id
+//点击控件
+function mai(params = {}) {
     params.controlId = dictionary.controlId[params.controlName];
     const defaultParams = {
         projectId: dictionary.projectId, //*项目ID（根据该字段，匹配相应的埋点数据）StringprojectId;必填
-        pageId: "", //*功能板ID（根据该字段，匹配相应功能板块的埋点数据）StringpageId;
-        controlId: "", //*控件名（根据该字段，匹配相应的控件）StringcontrolName;必填
-        userName: localStorage.getItem('code') || '', //用户名StringuserName;
-        detail: "", //其余参数储存Stringdetail
-        operationId: "", //操作ID（根据该字段，匹配相应的操作）StringoperationId;
-        brand: "", //品牌名/活动名/自定义标签（若没有可不填）Stringbrand;
-        channel: "", //媒体渠道（若没有可不填）Stringchannel;
+        pageId: '', //*功能板ID（根据该字段，匹配相应功能板块的埋点数据）StringpageId;
+        controlId: '', //*控件名（根据该字段，匹配相应的控件）StringcontrolName;必填
+        userName: '', //用户名StringuserName;
+        detail: '', //其余参数储存Stringdetail
+        operationId: '', //操作ID（根据该字段，匹配相应的操作）StringoperationId;
+        brand: '', //品牌名/活动名/自定义标签（若没有可不填）Stringbrand;
+        channel: '', //媒体渠道（若没有可不填）Stringchannel;
         amount: 0, //金额（若没有可不填）Moneyamount;没有就写0
-        beginTime: "", //数据请求时间范围中的开始时间（若没有可不填）DatebeginTime;
-        endTime: "", //数据请求时间范围中的结束时间（若没有可不填）DateendTime;
-        operationTime: "", //操作时间（若没有可不填）DateoperationTime;
-        ip: "", //IP地址（若没有可不填）Stringip;
-        userSource: "", //用户来源（若没有可不填）StringuserSource;
-        equipment: dictionary.equipment || "", //设备（PC、APP、H5）Stringequipment;
-        city: "" //用户所在城市Stringcity;
+        beginTime: '', //数据请求时间范围中的开始时间（若没有可不填）DatebeginTime;
+        endTime: '', //数据请求时间范围中的结束时间（若没有可不填）DateendTime;
+        operationTime: '', //操作时间（若没有可不填）DateoperationTime;
+        ip: '', //IP地址（若没有可不填）Stringip;
+        userSource: '', //用户来源（若没有可不填）StringuserSource;
+        equipment: dictionary.equipment || '', //设备（PC、APP、H5）Stringequipment;
+        city: '' //用户所在城市Stringcity;
     };
     const patchParams = Object.assign(defaultParams, params);
-    console.log("埋点参数：", patchParams);
+    console.log('埋点参数：', patchParams);
     return axios({
-        url: "http://139.198.0.141:81/EventTracking/operation/add",
-        method: "post",
+        url: `${http}/operation/add`,
+        method: 'post',
         data: qs.stringify(patchParams)
     });
 }
-
+//页面访问
 function maiVisit(params = {}) {
     params.pageId = dictionary.pageId[params.pageName];
+
     const defaultParams = {
-        visitId: "",
-        pageId: "",
-        visit_time: "",
-        ip: "",
-        city: ""
+        visitId: '',
+        pageId: '',
+        visit_time: '',
+        ip: '',
+        city: ''
     };
     const patchParams = Object.assign(defaultParams, params);
-    console.log("埋点访问页面参数：", patchParams);
+    console.log('埋点访问页面参数：', patchParams);
 
     return axios({
-        url: "http://139.198.0.141:81/EventTracking/visit/add",
-        method: "post",
+        url: `${http}/visit/add`,
+        method: 'post',
         data: qs.stringify(patchParams)
     });
 }
--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,18 @@
+const Timestamp = new Date().getTime();
+
 module.exports = {
     publicPath: './',
     outputDir: 'dist',
 
+    configureWebpack: {
+        output: {
+            // 输出重构【模块名称.hash值.时间戳】
+            filename: `[name].[hash].${Timestamp}.js`,
+            chunkFilename: `[name].[hash].${Timestamp}.js`
+        }
+    },
+
     lintOnSave: false,  //关闭eslint校验
 
+    productionSourceMap:false, //压缩打包体积
 }
