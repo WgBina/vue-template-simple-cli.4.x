832fc4953e914435ea292c06386f6417ed6740fa
bing.wan <1078827038@qq.com>
2020-11-16 14:50:30 +0800

    auth中coolie存储改为localstorage

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,28 +1,25 @@
-import Cookies from 'js-cookie'
-
-const TokenKey = 'Admin-Token'
-const RolesKey = 'Admin-Roles'
+const TokenKey = 'creativeX_roleId';
+const NameKey = 'creativeX_name';
 
 export function getToken() {
-  return Cookies.get(TokenKey)
+    return localStorage.getItem(TokenKey);
 }
 
 export function setToken(token) {
-  return Cookies.set(TokenKey, token)
+    return localStorage.setItem(TokenKey, token);
 }
 
 export function removeToken() {
-  return Cookies.remove(TokenKey)
+    return localStorage.removeItem(TokenKey);
 }
 
-export function getRoles() {
-  return Cookies.get(RolesKey)
+export function getName() {
+    return localStorage.getItem(NameKey);
 }
 
-export function setRoles(token) {
-  return Cookies.set(RolesKey, token)
+export function setName(name) {
+    return localStorage.setItem(NameKey, name);
 }
-
-export function removeRoles() {
-  return Cookies.remove(RolesKey)
+export function removeName() {
+    return localStorage.removeItem(NameKey);
 }
